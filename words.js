188d801353be9bf7ba4ce12a6d6d728b119d47f0
Cleo Souza <cleosouza699@gmail.com>
2024-05-17 17:27:40 -0300

    jogo de adivinhação de palavras

---
+++ words.js
@@ -0,0 +1,236 @@
+let wordList = [
+
+{
+    "word": "banana",
+    "hint": "fruta amarela"
+},
+{
+    "word": "mel",
+    "hint": "doce"
+},
+{
+    "word": "github",
+    "hint": "plataforma de hospedagem de código"
+},
+{
+    "word": "png",
+    "hint": "um formato de arquivo de imagem"
+},
+{
+    "word": "prata",
+    "hint": "metal precioso acinzentado-branco"
+},
+{
+    "word": "fone",
+    "hint": "dispositivo eletrônico"
+},
+{
+    "word": "gpu",
+    "hint": "componente de computador"
+},
+{
+    "word": "java",
+    "hint": "linguagem de programação"
+},
+{
+    "word": "google",
+    "hint": "famoso mecanismo de busca"
+},
+{
+    "word": "veneza",
+    "hint": "famosa cidade das águas"
+},
+{
+    "word": "excel",
+    "hint": "produto da Microsoft para Windows"
+},
+{
+    "word": "danca",
+    "hint": "mover-se ao som da música"
+},
+{
+    "word": "aguia",
+    "hint": "ave de rapina"
+},
+{
+    "word": "acerola",
+    "hint": "fruta pequena"
+},
+{
+    "word": "calcio",
+    "hint": "vitamina"
+},
+{
+    "word": "gelatina",
+    "hint": "geleia de frutas"
+},
+{
+    "word": "lemao",
+    "hint": "citrino azedo"
+},
+{
+    "word": "melao",
+    "hint": "fruta doce"
+},
+{
+    "word": "uva",
+    "hint": "fruta pequena"
+},
+{
+    "word": "pera",
+    "hint": "fruta macia"
+},
+{
+    "word": "rainha",
+    "hint": "governante feminina"
+},
+{
+    "word": "coelho",
+    "hint": "animal peludo"
+},
+{
+    "word": "aranha",
+    "hint": "inseto de oito patas"
+},
+{
+    "word": "tigre",
+    "hint": "grande felino"
+},
+{
+    "word": "tio",
+    "hint": "irmão do pai"
+},
+{
+    "word": "virose",
+    "hint": "doença"
+},
+{
+    "word": "agua",
+    "hint": "líquido transparente"
+},
+{
+    "word": "xenomio",
+    "hint": "elemento químico"
+},
+{
+    "word": "iate",
+    "hint": "barco"
+},
+{
+    "word": "pao",
+    "hint": "alimento assado"
+},
+{
+    "word": "queijo",
+    "hint": "produto lácteo"
+},
+{
+    "word": "ponei",
+    "hint": "parente do cavalo"
+},
+{
+    "word": "gemada",
+    "hint": "bebida(ovo)"
+},
+{
+    "word": "geladeira",
+    "hint": "eletrodoméstico"
+},
+{
+    "word": "guitarra",
+    "hint": "instrumento de cordas"
+},
+{
+    "word": "hockey",
+    "hint": "esporte no gelo"
+},
+{
+    "word": "requijao",
+    "hint": "produto lácteo"
+},
+{
+    "word": "lobo",
+    "hint": "parente do cachorro"
+},
+{
+    "word": "laranja",
+    "hint": "nome de cor e fruta"
+},
+{
+    "word": "fogao",
+    "hint": "eletrodoméstico"
+},
+{
+    "word": "flauta",
+    "hint": "instrumento de sopro"
+},
+{
+    "word": "futebol",
+    "hint": "esporte coletivo"
+},
+{
+    "word": "manteiga",
+    "hint": "produto lácteo"
+},
+{
+    "word": "ponei",
+    "hint": "parente do cavalo"
+},
+{
+    "word": "gemada",
+    "hint": "bebida(ovo)"
+},
+{
+    "word": "celular",
+    "hint": "eletronico"
+},
+{
+    "word": "judo",
+    "hint": "artes maciais "
+},
+{
+    "word": "floral",
+    "hint": "fragrancia"
+},
+{
+    "word": "entardecer",
+    "hint": "por do sol"
+},
+{
+    "word": "vinho",
+    "hint": "bebida(milagre)"
+},
+{
+    "word": "granizo",
+    "hint": "chuva"
+},
+{
+    "word": "trovao",
+    "hint": "chuva"
+},
+{
+    "word": "bicicleta",
+    "hint": "pedal"
+},
+{
+    "word": "flamengo",
+    "hint": "time de futebol"
+},
+{
+    "word": "cafe",
+    "hint": "bebida matinal"
+},
+{
+    "word": "sol",
+    "hint": "estrla"
+},
+{
+    "word": "web",
+    "hint": "programação"
+},
+{
+    "word": "sul",
+    "hint": "polo norte..."
+},
+
+]
